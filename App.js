1d7d2537f23585bc5e9725f8b1ecb326e3212623
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2023-11-01 12:06:57 +0900

    setting  up fonts

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/regular.otf"),
+    medium: require("./assets/fonts/medium.otf"),
+    bold: require("./assets/fonts/bold.otf"),
+    lights: require("./assets/fonts/light.otf"),
+    xtrabold: require("./assets/fonts/xtrabold.otf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    fontFamily: "xtrabold",
+    fontSize: 16,
   },
 });
