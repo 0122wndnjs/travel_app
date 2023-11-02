373a040e5da9868764810aaa35ab81af86622f7e
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2023-11-02 15:09:33 +0900

    Recommendations Page Working

--- components/index.js
+++ components/index.js
@@ -3,11 +3,11 @@ import ReusableText from "./Reusable/ReusableText";
 import ReusableBtn from "./Buttons/ReusableBtn";
 import HeightSpacer from "./Reusable/HeightSpacer";
 import WidthSpacer from "./Reusable/WidthSpacer";
-import Places from "./Home/Places";
 import Country from "./Tiles/Country/Country";
 import NetworkImage from "./Reusable/NetworkImage";
 import Recommendations from "./Home/Recommendations";
 import ReusableTile from "./Reusable/ReusableTile";
+import Rating from "./Reusable/Rating";
 
 export {
   Slides,
@@ -15,9 +15,9 @@ export {
   ReusableBtn,
   HeightSpacer,
   WidthSpacer,
-  Places,
   Country,
   NetworkImage,
   Recommendations,
   ReusableTile,
+  Rating,
 };
--- screens/home/Home.jsx
+++ screens/home/Home.jsx
@@ -2,7 +2,8 @@ import { Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import reusable from "../../components/Reusable/reusable.style";
-import { HeightSpacer, ReusableText, Places } from "../../components";
+import { HeightSpacer, Recommendations, ReusableText } from "../../components";
+import Places from '../../components/Home/Places'
 import { COLORS, SIZES, TEXT } from "../../constants/theme";
 import { AntDesign } from "@expo/vector-icons";
 import styles from "./home.style";
@@ -35,7 +36,7 @@ const Home = ({ navigation }) => {
         />
         <Places />
         <HeightSpacer height={15} />
-        
+        <Recommendations />
       </View>
     </SafeAreaView>
   );
