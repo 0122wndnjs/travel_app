de15c079a2c4abaecafecf433be066e48fe7713a
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2023-11-03 16:30:16 +0900

    Location is working with Map

--- screens/location/Location.jsx
+++ screens/location/Location.jsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Text } from "react-native";
+import React from "react";
+import MapView, { Marker } from "react-native-maps";
 
 const Location = () => {
+  const coordinates = {
+    latitude: 37.52890,
+    longitude: 126.83979,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+    title: "My Location",
+  };
   return (
-    <View>
-      <Text>Location</Text>
-    </View>
-  )
-}
+    <MapView initialRegion={coordinates} style={styles.mapStyle}>
+      <Marker coordinate={coordinates} title={coordinates.title} />
+    </MapView>
+  );
+};
 
-export default Location
+export default Location;
+
+const styles = StyleSheet.create({
+  mapStyle: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
