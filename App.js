2a3d0726ef8301ef99732ada88f0c71716452631
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2023-11-03 10:46:16 +0900

    Creating Recommendation and HotelList Page

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { OnBoarding, Search, CountryDetails, Recommended, PlaceDetails, HotelDetails, HotelList } from "./screens";
+import { OnBoarding, Search, CountryDetails, Recommended, PlaceDetails, HotelDetails, HotelList, HotelSearch } from "./screens";
 import BottomTabNavigation from "./navigation/BottomTabNavigation";
 
 const Stack = createNativeStackNavigator();
@@ -38,6 +38,7 @@ export default function App() {
         <Stack.Screen name="PlaceDetails" component={PlaceDetails} options={{ headerShown: false }} />
         <Stack.Screen name="HotelDetails" component={HotelDetails} options={{ headerShown: false }} />
         <Stack.Screen name="HotelList" component={HotelList} options={{ headerShown: false }} />
+        <Stack.Screen name="HotelSearch" component={HotelSearch} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
