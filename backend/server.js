daddebae27b5e072c0e635edbe70052676afbc47
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2023-11-06 16:35:52 +0900

    connecting to mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const port = 5003;
 
 dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(process.env.PORT || port, () =>
